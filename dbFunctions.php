69fbb671691b743f2fc0868e093d45948d50a06f
Gregory R. Everitt <fake@fake.com>
2014-03-13 22:09:16 +0000

    added simplistic MySQL library

---
+++ dbFunctions.php
@@ -0,0 +1,25 @@
+<?php
+
+function connect() {
+	$parameters = file('dbInfo.inc.php');
+	$db = new mysqli(trim($parameters[1]), trim($parameters[2]), trim($parameters[3]), trim($parameters[4]));
+    return $db;
+}
+
+function checkConnection($db) {
+	// check connection for errors 
+	if ($db->connect_errno) {
+	printf("Connection failed: %s\n", $db->connect_errno);
+	exit();
+	}
+}
+
+// executes package of functions so that each source file need only call one function to get access to MySQL
+function getDBAccess() {
+	$db = connect();
+	checkConnection($db);
+	ensureTableInit($db);
+	return $db;
+}
+
+?>
